d28cba3437a6fa7786d6949d0ea5fa820a0b3366
vtitov <iqronaft@gmail.com>
2023-12-20 03:38:47 +0200

    init commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8294d0f7-3078-4bce-bf88-22eb0064d9f9';
+export const MODAL_CLOSE_SEC = 2.5;
